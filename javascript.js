57564835258b5370e35679deb79b29a31cf5410b
yuav8520 <yuav8520@gmail.com>
2024-03-06 23:43:56 +0200

    pretty sure i did it right only diagnal is left

--- javascript.js
+++ javascript.js
@@ -14,14 +14,55 @@ const GameBorad=(function(){
 return{addtile,print};
 
 })();
-const Players=(function(){
-const PlayerOne=() => "X";
-const PlayerTwo=() => "O";
-return{PlayerOne,PlayerTwo};
+const Player=function(name,marker){
+   const returnMarker=()=>{
+        return marker;}
+    const returnName=()=>{
+        return name;}    
+            return{returnMarker,returnName};
+        }();
+
+
+
+
+const flow =(function(){//player one starts than player two untill winner
+const counter=0;
+const ifWinner=(function (gameboard) {//recives an array and marker and returns if winner
+    let b=true;
+  for (let i = 0; i < 3; i++) {//each row all j=
+     const marker=gameboard[i][0]
+     if (marker!==null) {
+        for (let j = 1; j <3; j++) {// i  is row j is column
+            if(marker!==gameboard[i][j]) 
+            {   b=false;
+                break;
+            }
+          
+        }
+        if(b)//as long as b wasn't change to false;
+        {return marker;}
+        else b=true;
+     }
+  
+  }
+  for (let j = 0; j < 3; j++) {//each colum all i=
+    const marker=gameboard[0][j]
+    if (marker!==null) {
+       for (let i = 1; i <3; i++) {// i is row j is column
+           if(marker!==gameboard[i][j]) 
+           {   b=false;
+               break;
+           }
+         
+       }
+       if(b)//as long as b wasn't change to false;
+       {return marker;}
+       else b=true;
+    }
+ 
+ }
+})
+
+
 })();
-
-const flow =(function(){
-
-
-})()
 //console.log(Players.PlayerOne)
